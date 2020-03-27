d1ccba20820751139a2b967f6beb6ed528f6060f
Shawn Giroux <shawn.giroux90@gmail.com>
2020-03-27 09:33:51 -0400

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::io::Read;
+// use std::io::prelude::*;
+use std::fs::File;
+
+struct CPU {
+    // Chip 8 has 35 opcodes
+    // Each are 2 bytes long
+    opcode: u16,
+
+    // Chip 8 has 4K memory
+    memory: [u16; 4096],
+
+    // CPU Registers
+    V: [u16; 16],
+
+    // Index Registers:
+    // I and Program Counter
+    I: u16,
+    pc: u16,
+
+    // Maintains current location
+    // before jumps are performed
+    stack: [u16; 16],
+    sp: u16,
+}
+
+impl CPU {
+    fn initialize(path: &str) -> CPU {
+        // Loading game file into buffer
+        let mut f = File::open(path).unwrap();
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer);
+
+        // Initializing and loading memory
+        let mut memory: [u16; 4096] = [0x0000; 4096];
+
+        for (i, _) in buffer.iter().enumerate() {
+            // println!("pos {}: {:#06x}", i, buf)
+            memory[i + 512] = buffer[i].into();
+        }
+
+        // for (i, buf) in memory.iter().enumerate() {
+        //     println!("pos {}: {:#06x}", i, buf)
+        // }
+
+        CPU {
+            opcode: 0,
+            memory: memory,
+            V: [0; 16],
+            I: 0,
+            pc: 0x200,
+            stack: [0x0000; 16],
+            sp: 0
+        }
+    }
+
+    fn emulate_cycle(&mut self) {
+        // Opcodes are stored in two memory locations
+        // We need both to get the full opcode
+        let opcode_pt_2 = self.pc as usize;
+        let opcode_pt_1 = (self.pc + 1) as usize;
+        self.opcode = self.memory[opcode_pt_1] << 8 | self.memory[opcode_pt_2];
+
+        let decode = self.opcode & 0xF000;
+        match decode {
+            0x0000 => {
+                CPU::debug_opcode(self.opcode, decode);
+            }
+            0xA000 => {
+                self.I = self.opcode & 0x0FFF;
+                self.pc += 2;
+            },
+            _ => {
+                println!("Undetermined Opcode!");
+                CPU::debug_opcode(self.opcode, decode);
+            }
+        }
+    }
+
+    fn debug_opcode(opcode: u16, decode: u16) {
+        println!("Opcode: {:#06x}", opcode);
+        println!("Decode: {:#06x}", decode)
+    }
+}
+
+fn main() {
+    let path = "/home/calico/Programming/rust/rusty-chip8/pong.ch8";
+    let mut cpu = CPU::initialize(path);
+    cpu.emulate_cycle();
+}
